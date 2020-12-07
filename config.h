68a5f2c954ce00f681580c4154f616f249c9ffa1
shiftgeist <git@shiftgeist.com>
2020-12-07 16:07:26 +0100

    update config

--- config.h
+++ config.h
@@ -9,11 +9,11 @@ static const int vertpad            = 0;       /* vertical padding of bar */
 static const int sidepad            = 0;       /* horizontal padding of bar */
 static const char *fonts[]          = { "monospace:size=10" };
 static const char dmenufont[]       = "monospace:size=10";
-static const char col_gray1[]       = "#222222";
-static const char col_gray2[]       = "#444444";
+static const char col_gray1[]       = "#292A2B";
+static const char col_gray2[]       = "#737373";
 static const char col_gray3[]       = "#bbbbbb";
-static const char col_gray4[]       = "#eeeeee";
-static const char col_cyan[]        = "#005577";
+static const char col_gray4[]       = "#f3f3f3";
+static const char col_cyan[]        = "#19F9D8";
 static const unsigned int baralpha = 0xd0;
 static const unsigned int borderalpha = OPAQUE;
 static const char *colors[][3]      = {
